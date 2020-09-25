9eccb63ed4adc7b7f5c852410fd80e0acb3ca3f6
andrewglevy <andrewglevy@gmail.com>
2020-09-24 22:54:04 -0700

    add elections controller with basic crud

---
+++ src/controllers/elections.controller.js
@@ -0,0 +1,106 @@
+import Election from '../models/election';
+
+export async function getElections(req, res) {
+    try {
+        const getData = await Election.findAll(req.body)
+        if (getData) {
+            res.json({
+                success: true,
+                message: "Election Fetch Successful",
+                data: getData
+            });
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            success: false,
+            message: "something went wrong"
+        });
+    }
+}
+export async function getElection(req, res) {
+    try {
+        const createData = await Election.findOne({ where: req.body });
+        if (createData) {
+            res.json({
+                success: true,
+                message: "Election fetch Successfull",
+                data: createData
+            });
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            success: false,
+            message: "Something went wrong!"
+        });
+    }
+}
+
+export async function createElection(req, res) {
+    try {
+        const checkData = await Election.findOne({ where: { id: req.body.id } });
+        if (checkData) {
+            res.json({
+                message: "Already Exists",
+                data: checkData
+            });
+        } else {
+            const createData = await Election.create(req.body, { fields: ['name'] });
+            if (createData) {
+                res.json({
+                    success: true,
+                    message: "Election Created Successfully",
+                    data: createData
+                });
+            }
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            success: false,
+            message: "Something went wrong!"
+        });
+    }
+}
+
+export async function deleteElection(req, res) {
+    try {
+        const deleteData = await Election.destroy({ where: { id: req.body.id } });
+        if (deleteData) {
+            res.json({
+                success: true,
+                message: "Election Deleted Successfully",
+                data: deleteData
+            });
+        }
+    } catch (err) {
+        res.status(500).json({
+            success: false,
+            message: "Something went wrong!"
+        });
+    }
+}
+
+export async function updateElections(req, res) {
+    try {
+        const findData = await Election.findAll({ where: { id: req.body.id } });
+        if (findData.length > 0) {
+            findData.forEach(async data => { await data.update(req.body) })
+        }
+        return res.json({
+            success: true,
+            message: "Election Updated Successfully",
+            data: findData
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            success: false,
+            message: "Something went wrong!"
+        });
+    }
+}
+
+
+
