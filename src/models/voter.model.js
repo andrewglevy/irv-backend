62b67ab0d0e81149d04838d1f6742d2642f72c0d
andrewglevy <andrewglevy@gmail.com>
2020-09-26 14:09:51 -0700

    add voters table with voter code moved from ballot

---
+++ src/models/voter.model.js
@@ -0,0 +1,22 @@
+export default function Voter(sequelize, DataTypes) {
+    return sequelize.define('voters', {
+        name: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: true,
+            validate: { isEmail: true }
+        },
+        whenVoted: {
+            type: DataTypes.DATE,
+            allowNull: true
+        },
+        voterCode: {
+            // only exists if election.emailBallots === true, else voter will use election code
+            type: DataTypes.STRING,
+            allowNull: true,
+        }
+    });
+}
