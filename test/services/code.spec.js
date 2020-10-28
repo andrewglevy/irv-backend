52af073358360699800e013840f0ff98e3937c2f
andrewglevy <andrewglevy@gmail.com>
2020-10-28 00:03:01 -0700

    add test for code.service makeCode() method

--- test/services/code.spec.js
+++ test/services/code.spec.js
@@ -23,16 +23,30 @@ describe('CodeService', () => {
             done();
         });
         it('should return true if the code is a duplicate', async done => {
-            const code = new Code(votersTable, 'voterCode', 6)
+            const code = new Code(votersTable, 'voterCode');
             const result = await code.isDuplicate('QUERTY');
             expect(result).toBe(true);
             done();
         });
         it('should return false if the code is not a duplicate', async done => {
-            const code = new Code(votersTable, 'voterCode', 6)
+            const code = new Code(votersTable, 'voterCode');
             const result = await code.isDuplicate('ASDFGH');
             expect(result).toBe(true);
             done();
         });
     });
+    describe('makeCode', () => {
+        it('should generate a random letter code of the length specified', () => {
+            const code = new Code();
+            const sixLetterCode = code.makeCode(6);
+            const allCharsLetters = () => {
+                for (let char of sixLetterCode) {
+                    if(!'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.includes(char)) return false;
+                    return true;
+                }
+            }
+            expect(sixLetterCode.length).toEqual(6)
+            expect(allCharsLetters()).toBe(true);
+        });
+    })
 });
