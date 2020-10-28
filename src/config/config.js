e791f8e1852d3a0ee09c449e8e25b9be09e9a411
andrewglevy <andrewglevy@gmail.com>
2020-10-27 17:51:41 -0700

    handle schemas for different environments

--- src/config/config.js
+++ src/config/config.js
@@ -2,11 +2,24 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
+const setSchema = (environment) => {
+  switch (environment) {
+    case 'dev':
+      return process.env.DEV_SCHEMA;
+    case 'test':
+      return process.env.TEST_SCHEMA;
+    // case 'prod':
+      // prod schema
+    default:
+      return process.env.DEV_SCHEMA;
+  }
+}
+
 const PORT = process.env.PORT || 3000;
 const HOSTNAME = process.env.HOSTNAME || 'localhost';
 const DB_USER = process.env.DB_USER;
 const DB_PASS = process.env.DB_PASS;
-const DB_SCHEMA = process.env.DB_SCHEMA;
+const DB_SCHEMA = setSchema(process.env.NODE_ENV);
 
 export default {
   server: {
