e7b5bea402507199f0c3559de70bf944a6f1aa3e
andrewglevy <andrewglevy@gmail.com>
2020-10-27 23:44:00 -0700

    add tests for code service, isDuplicate() method

---
+++ test/services/code.spec.js
@@ -0,0 +1,38 @@
+import Code from '../../src/services/code.service';
+import db from '../../src/models/index';
+
+const votersTable = db.voters;
+
+describe('CodeService', () => {
+    beforeAll(async done => {
+        await db.sequelize.sync({ force: true })
+        done();
+    });
+
+    afterAll(async done => {
+        db.sequelize.close();
+        done();
+    });
+    describe('isDuplicate()', () => {
+        beforeAll(async done => {
+            await votersTable.create({
+                name: 'George',
+                email: 'george@example.com',
+                voterCode: 'QWERTY'
+            });
+            done();
+        });
+        it('should return true if the code is a duplicate', async done => {
+            const code = new Code(votersTable, 'voterCode', 6)
+            const result = await code.isDuplicate('QUERTY');
+            expect(result).toBe(true);
+            done();
+        });
+        it('should return false if the code is not a duplicate', async done => {
+            const code = new Code(votersTable, 'voterCode', 6)
+            const result = await code.isDuplicate('ASDFGH');
+            expect(result).toBe(true);
+            done();
+        });
+    });
+});
