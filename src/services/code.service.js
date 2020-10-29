893da38d718898d0c5f77f97f76b7ef7ce2015b9
andrewglevy <andrewglevy@gmail.com>
2020-10-29 14:28:56 -0700

    update code service

--- src/services/code.service.js
+++ src/services/code.service.js
@@ -19,25 +19,18 @@ export default class Code {
     }
 
     async isDuplicate(code) {
-        try {
-            const checkData = await this.getTable().findOne({ where: { [this.getProperty()]: [code] } });
-            if (checkData) return true;
-            return false;
-        } catch (err) {
-            console.log(err);
-        }
+        const checkData = await this.getTable().findOne({ where: { [this.getProperty()]: [code] } });
+        if (checkData) return true;
+        return false;
     }
 
     async addCode() {
         let duplicate, code;
-        try {
-            do {
-                code = this.makeCode(this.getLength())
-                duplicate = await this.isDuplicate(code);
-            } while (duplicate);
-            return code;
-        } catch (err) {
-            console.log(err);
-        }
+        do {
+            code = this.makeCode(this.getLength())
+            duplicate = await this.isDuplicate(code);
+        } while (duplicate);
+        return code;
+
     }
 }
